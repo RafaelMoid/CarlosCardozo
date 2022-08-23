468ecb41f4e3c92d56046e13ff8334e2c5591fa8
Rafael <rafaelvrcr@gmail.com>
2022-08-23 15:31:18 -0300

    [Feat] Header/Section 1

--- header.php
+++ header.php
@@ -10,9 +10,8 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&family=Titillium+Web:wght@300;400;600;700;900&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<!-- <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/home.js" defer></script> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
 
 
 
@@ -24,16 +23,24 @@
 
 <body>
 	<header class="header">
-		<div class="header-img" >
+		<div class="header-img">
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/cc-logo.png" alt="logo" class="logo">
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/cc-foto.png" alt="foto" class="foto">
 		</div>
 		<div class="header-txt">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/cc-trans-destaque.png" alt="">
+			<img class="bg-header" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/cc-trans-destaque.png" alt="">
 			<h1>MÚSICA BRASILEIRA DE ORIGEM E ESTILO</h1>
-			<P>Conheça a trajetória do músico Carlinhos Cardozo and banda.</P>
-			<button>DISCOVER NEW ALBUM</button>
-			<button>DISCOVER MY HISTORY</button>
+			<p>Conheça a trajetória do músico Carlinhos Cardozo and banda.</p>
+			<div class="btn-wrapper">
+				<button class="btn-header-1">DISCOVER NEW ALBUM</button>
+				<button class="btn-header-2">DISCOVER MY HISTORY</button>
+			</div>
+			<ul class="social">
+				<li><a href="#" class="fa fa-instagram"></a></li>
+				<li><a href="#" class="fa fa-facebook"></a></li>
+				<li><a href="#" class="fa fa-linkedin"></a></li>
+				<li><a href="#" class="fa fa-whatsapp"></a></li>
+			</ul>
 		</div>
 
 	</header>
