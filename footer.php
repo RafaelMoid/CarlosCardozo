fbfa98c5ddddd51695ac50a75384b9d247c194a4
Rafael <rafaelvrcr@gmail.com>
2022-08-25 20:34:09 -0300

    [Feat] Footer

--- footer.php
+++ footer.php
@@ -1,11 +1,19 @@
         <footer class="footer">
+            <!-- Footer Wordpress -->
+            <div class="footer-section">
 
+                <div class="logo-box">
+                    <p>Developed by</p>
+                    <a href="https://jazzz.com.br" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/JAZZZ.png" alt="Logo da Jazz"></a>
+                </div>
+            </div>
         </footer>
         <!-- Footer Wordpress -->
         <?php wp_footer(); ?>
         <!-- Fecha Footer Wordpress -->
 
 
-        
-    </body>
-</html>
+
+        </body>
+
+        </html>
